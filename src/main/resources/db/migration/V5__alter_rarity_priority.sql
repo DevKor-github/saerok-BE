e94f6dd3834dffa95225641af8c1ca7fa80b6ef1
soonduck <nederileni99heits@gmail.com>
2025-04-30 14:15:37 +0900

    feat(infra): 도감 이미지용 S3 버킷에 CloudFront 설정 및 이에 따른 이미지 데이터 변경

--- src/main/resources/db/migration/V5__alter_rarity_priority.sql
+++
@@ -1,9 +0,0 @@
--- 기존에는 새의 특정 계절 희귀도를 결정할 때 UNSPECIFIED를 RARE보다 우선시했는데, RARE를 우선시하도록 변경함
-
-UPDATE rarity_type
-SET priority = CASE code
-                   WHEN 'UNSPECIFIED' THEN -10
-                   WHEN 'RARE' THEN 0
-                   ELSE priority
-    END
-WHERE code IN ('UNSPECIFIED', 'RARE');
