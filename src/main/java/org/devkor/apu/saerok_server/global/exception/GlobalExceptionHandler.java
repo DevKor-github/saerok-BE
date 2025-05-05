e222375a03dcb3b1efb7852d237fe23a2766ccb8
Junhee Han <115782193+soonduck-dreams@users.noreply.github.com>
2025-05-05 20:44:21 +0900

    feat(dex): 도감 검색/조회 API에 정렬 기능 추가 (#18)
    * chore: BadRequestException 추가
    
    * chore: build.gradle 설정 정리
    
    필요없는 QueryDSL 관련 설정 제거,
    MapStruct 매퍼 구현체가 반복해서 만들어지는 현상 억제
    
    * feat: 도감 조회/검색 API에 정렬 기능 추가

--- src/main/java/org/devkor/apu/saerok_server/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/devkor/apu/saerok_server/global/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package org.devkor.apu.saerok_server.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -13,6 +15,14 @@ public class GlobalExceptionHandler {
         ErrorResponse error = new ErrorResponse(500, ex.getMessage());
         return ResponseEntity.
                 internalServerError().
+                body(error);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(BadRequestException ex) {
+        ErrorResponse error = new ErrorResponse(400, ex.getMessage());
+        return ResponseEntity.
+                badRequest().
                 body(error);
     }
     
