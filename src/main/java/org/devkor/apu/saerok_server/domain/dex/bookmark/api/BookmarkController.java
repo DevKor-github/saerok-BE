6fd7a6ef63d3619a7bad4c1ca5dc614743f00e6a
pizzazoa <gkswns0716@naver.com>
2025-05-10 23:52:41 +0900

    docs(dex): 북마크 api 명세 변경

--- src/main/java/org/devkor/apu/saerok_server/domain/dex/bookmark/api/BookmarkController.java
+++ src/main/java/org/devkor/apu/saerok_server/domain/dex/bookmark/api/BookmarkController.java
@@ -1,17 +1,8 @@
 package org.devkor.apu.saerok_server.domain.dex.bookmark.api;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
-import org.devkor.apu.saerok_server.domain.dex.bookmark.api.dto.response.BookmarkIdResponse;
-import org.devkor.apu.saerok_server.domain.dex.bookmark.api.dto.response.BookmarkResponse;
 import org.devkor.apu.saerok_server.domain.dex.bookmark.application.BookmarkService;
-import org.devkor.apu.saerok_server.global.security.UserPrincipal;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,36 +15,37 @@ public class BookmarkController {
     @GetMapping
     @Operation(
             summary = "[미구현] 내 북마크 목록 조회",
-            description = "사용자가 북마크한 조류 목록을 조회합니다."
+            description = "사용자가 북마크한 조류 목록을 조회합니다. " +
+                    "북마크 엔티티의 정보만 포함합니다."
     )
     public void getBookmarks() {
         // 미구현
     }
 
-    @GetMapping("/{birdId}")
+    @GetMapping("/items")
     @Operation(
-            summary = "[미구현] 특정 조류 북마크 여부 조회",
-            description = "사용자가 해당 조류를 북마크했는지 여부를 조회합니다."
+            summary = "[미구현] 북마크한 조류 상세 정보 조회",
+            description = "사용자가 북마크한 조류들의 상세 정보를 조회합니다."
     )
-    public void isBookmarked() {
+    public void getBookmarkedBirdDetails() {
         // 미구현
     }
 
-    @PostMapping("/{birdId}")
+    @PostMapping("/{birdId}/toggle")
     @Operation(
-            summary = "[미구현] 북마크 추가",
-            description = "사용자가 특정 조류를 북마크합니다."
+            summary = "[미구현] 조류 북마크 토글",
+            description = "특정 조류에 대한 북마크를 추가하거나 제거합니다."
     )
-    public void addBookmark() {
+    public void postBookmarkToggle() {
         // 미구현
     }
 
-    @DeleteMapping("/{birdId}")
+    @GetMapping("/{birdId}/status")
     @Operation(
-            summary = "북마크 해제",
-            description = "사용자가 특정 조류의 북마크를 해제합니다."
+            summary = "[미구현] 조류 북마크 상태 확인",
+            description = "특정 조류에 대한 북마크 상태를 확인합니다."
     )
-    public void removeBookmark() {
-        //미구현
+    public void getBookmarkStatus() {
+        // 미구현
     }
 }
---
+++ src/main/java/org/devkor/apu/saerok_server/domain/dex/bookmark/api/dto/response/BookmarkStatusResponse.java
@@ -0,0 +1,4 @@
+package org.devkor.apu.saerok_server.domain.dex.bookmark.api.dto.response;
+
+public class BookmarkStatusResponse {
+}
