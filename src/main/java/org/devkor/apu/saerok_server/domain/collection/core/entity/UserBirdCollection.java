052baeed18ddfe50b3483d2c830696d1196864a9
soonduck-dreams <nederileni99heits@gmail.com>
2025-05-17 22:41:37 +0900

    feat(entity): 컬렉션 및 컬렉션 이미지 엔티티 수정
    엔티티 생성을 위해 Builder 패턴 생성자 추가했음.
    UserBirdCollectionImage 엔티티에 contentType 속성 추가하여, 업로드한 이미지의 MIME type 기록

--- src/main/java/org/devkor/apu/saerok_server/domain/collection/core/entity/UserBirdCollection.java
+++ src/main/java/org/devkor/apu/saerok_server/domain/collection/core/entity/UserBirdCollection.java
@@ -1,25 +1,34 @@
 package org.devkor.apu.saerok_server.domain.collection.core.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.devkor.apu.saerok_server.domain.dex.bird.core.entity.Bird;
 import org.devkor.apu.saerok_server.domain.user.core.entity.User;
 import org.devkor.apu.saerok_server.global.entity.Auditable;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import org.locationtech.jts.geom.Point;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class UserBirdCollection extends Auditable {
+
+    public static final int NOTE_MAX_LENGTH = 50;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "bird_id", nullable = true)
     private Bird bird;
 
@@ -42,6 +51,25 @@ public class UserBirdCollection extends Auditable {
     private boolean isPinned;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "access_level", nullable = false)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(name = "access_level", nullable = false, columnDefinition = "access_level_enum")
     private AccessLevelType accessLevel;
+
+    @Builder
+    public UserBirdCollection(User user, Bird bird, String tempBirdName, LocalDate discoveredDate, Point location, String locationAlias, String note, boolean isPinned, AccessLevelType accessLevel) {
+
+        if (user == null) throw new IllegalArgumentException("user는 null일 수 없습니다.");
+        if (discoveredDate == null) throw new IllegalArgumentException("discoveredDate는 null일 수 없습니다.");
+        if (location == null) throw new IllegalArgumentException("location은 null일 수 없습니다.");
+
+        this.user = user;
+        this.bird = bird;
+        this.tempBirdName = tempBirdName;
+        this.discoveredDate = discoveredDate;
+        this.location = location;
+        this.locationAlias = locationAlias;
+        this.note = note;
+        this.isPinned = isPinned;
+        this.accessLevel = accessLevel == null ? AccessLevelType.PUBLIC : accessLevel;
+    }
 }
