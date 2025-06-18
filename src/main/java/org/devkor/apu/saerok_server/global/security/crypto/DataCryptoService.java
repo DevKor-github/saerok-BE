dfe173a4fac9779bbf6289ec4a79667f79055bf7
soonduck <nederileni99heits@gmail.com>
2025-06-18 21:00:12 +0900

    chore: 체크 예외를 try-catch로 래핑

--- src/main/java/org/devkor/apu/saerok_server/global/security/crypto/DataCryptoService.java
+++ src/main/java/org/devkor/apu/saerok_server/global/security/crypto/DataCryptoService.java
@@ -1,14 +1,7 @@
 package org.devkor.apu.saerok_server.global.security.crypto;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 public interface DataCryptoService {
 
-    EncryptedPayload encrypt(byte[] plaintext) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException;
-    byte[] decrypt(EncryptedPayload payload) throws IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException;
+    EncryptedPayload encrypt(byte[] plaintext);
+    byte[] decrypt(EncryptedPayload payload);
 }
--- src/main/java/org/devkor/apu/saerok_server/global/security/crypto/KmsDataCryptoService.java
+++ src/main/java/org/devkor/apu/saerok_server/global/security/crypto/KmsDataCryptoService.java
@@ -9,16 +9,10 @@ import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.awssdk.services.kms.model.DataKeySpec;
 import software.amazon.awssdk.services.kms.model.GenerateDataKeyResponse;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.ByteBuffer;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Arrays;
 
@@ -33,42 +27,54 @@ public class KmsDataCryptoService implements DataCryptoService{
     private String cmkId;
 
     @Override
-    public EncryptedPayload encrypt(byte[] plaintext) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        // 데이터 키 생성 (평문 데이터 키 + 암호화된 데이터 키)
-        GenerateDataKeyResponse dk = kms.generateDataKey(r -> r
-                .keyId(cmkId)
-                .keySpec(DataKeySpec.AES_256));
-        byte[] pdk = dk.plaintext().asByteArray();
-        byte[] cdk = dk.ciphertextBlob().asByteArray();
+    public EncryptedPayload encrypt(byte[] plaintext) {
 
-        // AES-256-GCM 암호화
-        byte[] iv = randomBytes(12);
-        Cipher c = Cipher.getInstance("AES/GCM/NoPadding");
-        c.init(Cipher.ENCRYPT_MODE,
-                new SecretKeySpec(pdk, "AES"),
-                new GCMParameterSpec(128, iv));
-        byte[] ctWithTag = c.doFinal(plaintext);
+        try {
+            // 데이터 키 생성 (평문 데이터 키 + 암호화된 데이터 키)
+            GenerateDataKeyResponse dk = kms.generateDataKey(r -> r
+                    .keyId(cmkId)
+                    .keySpec(DataKeySpec.AES_256));
+            byte[] pdk = dk.plaintext().asByteArray();
+            byte[] cdk = dk.ciphertextBlob().asByteArray();
 
-        int tagLen = 16;
-        byte[] ct = Arrays.copyOfRange(ctWithTag, 0, ctWithTag.length - tagLen);
-        byte[] tag = Arrays.copyOfRange(ctWithTag, ctWithTag.length - tagLen, ctWithTag.length);
+            // AES-256-GCM 암호화
+            byte[] iv = randomBytes(12);
+            Cipher c = Cipher.getInstance("AES/GCM/NoPadding");
+            c.init(Cipher.ENCRYPT_MODE,
+                    new SecretKeySpec(pdk, "AES"),
+                    new GCMParameterSpec(128, iv));
+            byte[] ctWithTag = c.doFinal(plaintext);
 
-        return new EncryptedPayload(ct, cdk, iv, tag);
+            int tagLen = 16;
+            byte[] ct = Arrays.copyOfRange(ctWithTag, 0, ctWithTag.length - tagLen);
+            byte[] tag = Arrays.copyOfRange(ctWithTag, ctWithTag.length - tagLen, ctWithTag.length);
+
+            return new EncryptedPayload(ct, cdk, iv, tag);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
-    public byte[] decrypt(EncryptedPayload payload) throws IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
-        // 데이터 키 복호화
-        byte[] pdk = kms.decrypt(r -> r.ciphertextBlob(SdkBytes.fromByteArray(payload.encryptedDataKey())))
-                .plaintext().asByteArray();
+    public byte[] decrypt(EncryptedPayload payload) {
 
-        // AES-256-GCM 복호화
-        Cipher c = Cipher.getInstance("AES/GCM/NoPadding");
-        c.init(Cipher.DECRYPT_MODE, new SecretKeySpec(pdk,"AES"),
-                new GCMParameterSpec(128, payload.iv()));
-        byte[] ctWithTag = ByteBuffer.allocate(payload.ciphertext().length + payload.authTag().length)
-                .put(payload.ciphertext()).put(payload.authTag()).array();
-        return c.doFinal(ctWithTag);
+        try {
+            // 데이터 키 복호화
+            byte[] pdk = kms.decrypt(r -> r.ciphertextBlob(SdkBytes.fromByteArray(payload.encryptedDataKey())))
+                    .plaintext().asByteArray();
+
+            // AES-256-GCM 복호화
+            Cipher c = Cipher.getInstance("AES/GCM/NoPadding");
+            c.init(Cipher.DECRYPT_MODE, new SecretKeySpec(pdk,"AES"),
+                    new GCMParameterSpec(128, payload.iv()));
+            byte[] ctWithTag = ByteBuffer.allocate(payload.ciphertext().length + payload.authTag().length)
+                    .put(payload.ciphertext()).put(payload.authTag()).array();
+            return c.doFinal(ctWithTag);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     private byte[] randomBytes(int length) {
