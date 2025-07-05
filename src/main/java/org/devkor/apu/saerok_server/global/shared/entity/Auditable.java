31b28008e378eb4637023a3a540de159ceda647d
soonduck-dreams <nederileni99heits@gmail.com>
2025-07-05 16:13:10 +0900

    chore: createdAt, updatedAt을 위한 Getter 추가

--- src/main/java/org/devkor/apu/saerok_server/global/shared/entity/Auditable.java
+++ src/main/java/org/devkor/apu/saerok_server/global/shared/entity/Auditable.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import lombok.Getter;
 
 import java.time.OffsetDateTime;
 
 @MappedSuperclass
+@Getter
 public abstract class Auditable {
 
     @Column(name = "created_at", updatable = false)
