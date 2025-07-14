c6db28d6550b3f6142d1f12cc04242bbe587213e
pizzazoa <gkswns0716@naver.com>
2025-07-14 18:42:55 +0900

    feat(coll): 댓글 신고 API 구현

---
+++ src/main/java/org/devkor/apu/saerok_server/domain/collection/api/dto/response/ReportCollectionCommentResponse.java
@@ -0,0 +1,9 @@
+package org.devkor.apu.saerok_server.domain.collection.api.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record ReportCollectionCommentResponse(
+        @Schema(description = "신고 ID", example = "1")
+        Long reportId
+) {
+}
