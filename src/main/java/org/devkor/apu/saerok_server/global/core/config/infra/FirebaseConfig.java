0e36f60e1c40425e4528b747c8621f7571f04d48
pizzazoa <gkswns0716@naver.com>
2025-07-17 15:47:40 +0900

    chore: FCM 환경 설정

---
+++ src/main/java/org/devkor/apu/saerok_server/global/core/config/infra/FirebaseConfig.java
@@ -0,0 +1,37 @@
+package org.devkor.apu.saerok_server.global.core.config.infra;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class FirebaseConfig {
+
+    @Value("${firebase.admin.key-path}")
+    private String keyPath;
+
+    @PostConstruct
+    public void initializeFirebase() {
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
+                ClassPathResource resource = new ClassPathResource(keyPath.replace("classpath:", ""));
+                
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Firebase 초기화 중 오류가 발생했습니다.", e);
+        }
+    }
+}
