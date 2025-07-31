a856759f74554600f2e3e4062da98f5eade3ea6c
pizzazoa <gkswns0716@naver.com>
2025-07-31 18:53:56 +0900

    feat(db): 기존 유저들에 기본 프로필 배정 마이그레이션 추가

--- src/main/resources/db/migration-local/R__local_dummy_user.sql
+++ src/main/resources/db/migration-local/R__local_dummy_user.sql
@@ -1,16 +1,28 @@
-INSERT INTO users (id, nickname, email, signup_status, created_at, updated_at)
+INSERT INTO users (id, nickname, email, signup_status, joined_at, created_at, updated_at)
 VALUES (
            99999,
            'dummy.saerokuser',
            'fake-email@saerok.com',
            'COMPLETED',
            now(),
+           now(),
            now()
-       );
+       ) ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO user_role(id, user_id, role)
 VALUES (
             99999,
             99999,
             'USER'
-       )
+       ) ON CONFLICT (id) DO NOTHING;
+
+-- 더미 유저 기본 프로필 이미지 추가
+INSERT INTO user_profile_image (id, user_id, object_key, content_type, created_at, updated_at)
+VALUES (
+    99999,
+    99999,
+    'profile-images/default/default.png',
+    'image/png',
+    now(),
+    now()
+) ON CONFLICT (id) DO NOTHING;
