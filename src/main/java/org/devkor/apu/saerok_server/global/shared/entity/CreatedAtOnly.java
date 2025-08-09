965a57240dd1cc4b7bb00c29d214c8c3acb089ba
pizzazoa <gkswns0716@naver.com>
2025-08-09 17:59:39 +0900

    refactor: createdAtOnly에 Getter 추가

--- src/main/java/org/devkor/apu/saerok_server/global/shared/entity/CreatedAtOnly.java
+++ src/main/java/org/devkor/apu/saerok_server/global/shared/entity/CreatedAtOnly.java
@@ -3,10 +3,12 @@ package org.devkor.apu.saerok_server.global.shared.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
+import lombok.Getter;
 
 import java.time.OffsetDateTime;
 
 @MappedSuperclass
+@Getter
 public abstract class CreatedAtOnly {
 
     @Column(name = "created_at", updatable = false)
