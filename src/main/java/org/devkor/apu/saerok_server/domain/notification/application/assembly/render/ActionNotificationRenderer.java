cb1049c79e15ef606afa42070b3e5c4fa8d0a994
soonduck <nederileni99heits@gmail.com>
2025-08-15 01:08:52 +0900

    chore: 알림 내용 기획 명세에 맞추기

--- src/main/java/org/devkor/apu/saerok_server/domain/notification/application/assembly/render/ActionNotificationRenderer.java
+++ src/main/java/org/devkor/apu/saerok_server/domain/notification/application/assembly/render/ActionNotificationRenderer.java
@@ -15,19 +15,19 @@ public class ActionNotificationRenderer implements NotificationRenderer {
 
         return switch (a.action()) {
             case LIKE -> new RenderedMessage(
-                    a.actorName() + "님이 좋아요를 눌렀어요",   // inApp body
-                    "새 좋아요 알림",                         // push title
-                    "누가 내 새록을 좋아했는지 확인해보세요"     // push body
+                    a.actorName() + "님이 나의 새록을 좋아해요.",   // inApp body
+                    a.actorName(),                         // push title
+                    "나의 새록을 좋아해요."     // push body
             );
             case COMMENT -> new RenderedMessage(
-                    a.actorName() + "님이 댓글을 남겼어요",
-                    "새 댓글 알림",
-                    String.valueOf(a.extras().getOrDefault("comment", ""))
+                    a.actorName() + "님이 나의 새록에 댓글을 남겼어요. \"" + String.valueOf(a.extras().getOrDefault("comment", "")) +"\"",
+                    a.actorName(),
+                    "나의 새록에 댓글을 남겼어요. " +String.valueOf(a.extras().getOrDefault("comment", ""))
             );
             case SUGGEST_BIRD_ID -> new RenderedMessage(
-                    a.actorName() + "님이 동정 의견을 제안했어요",
-                    "새 동정 의견 알림",
-                    String.valueOf(a.extras().getOrDefault("suggestedName", ""))
+                    "두근두근! 새로운 의견이 공유되었어요. 확인해볼까요?",
+                    "동정 의견 공유",
+                    "두근두근! 새로운 의견이 공유되었어요. 확인해볼까요?"
             );
         };
     }
