40b20fd6e44336ec0a130ccbf05eecb6778a7411
Junhee Han <115782193+soonduck-dreams@users.noreply.github.com>
2025-08-21 11:14:28 +0900

    feat: 도감 업데이트 (#157)

---
+++ src/main/resources/db/migration/V54__update_bird_images.sql
@@ -0,0 +1,125 @@
+UPDATE public.bird_image
+SET object_key = 'raw/1c02316a-d5a1-48c7-b7f2-7256745a1266.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Swinhoe%27s_Minivet_%2825647391664%29.jpg',
+    updated_at = now()
+WHERE bird_id = 502 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/d4d43d46-43c8-4a39-a91f-5c46f3bc8ce7.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/6/68/Pheasant.jpg',
+    updated_at = now()
+WHERE bird_id = 48 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/e696df59-2087-4944-85c9-8c8dc30b684a.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/0/05/Pygmy_Woodpecker_%E0%A6%AE%E0%A7%87%E0%A6%9F%E0%A7%87%E0%A6%9F%E0%A7%81%E0%A6%AA%E0%A6%BF_%E0%A6%95%E0%A6%BE%E0%A6%A0%E0%A6%95%E0%A7%81%E0%A6%A1%E0%A6%BC%E0%A6%BE%E0%A6%B2%E0%A6%BF.jpg',
+    updated_at = now()
+WHERE bird_id = 156 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/fe81cd2b-3d79-4c12-b79c-058219af7138.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/7/79/%D0%A1%D0%B5%D0%B4%D0%BE%D0%B9_%D0%B4%D1%8F%D1%82%D0%B5%D0%BB_%D1%83_%D0%B1%D0%BE%D0%BB%D0%BE%D1%82%D0%B0_%D1%80%D0%B5%D1%87%D0%BA%D0%B8_%D0%97%D0%B8%D0%BC%D1%91%D0%BD%D0%BA%D0%B8.jpg',
+    updated_at = now()
+WHERE bird_id = 163 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/c1000a87-4160-4814-98d5-9ce074776fca.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Coloeus_dauuricus%2C_adult%2C_Izumi%2C_Kagoshima%2C_Japan_3.jpg',
+    updated_at = now()
+WHERE bird_id = 185 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/df910d20-68ad-46f1-a770-7bd42a04abf0.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Yellow_Bittern_at_Hyoko_crop.jpg',
+    updated_at = now()
+WHERE bird_id = 221 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/e03ae6c3-5f3f-4918-b47b-0cf24415b87a.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/2/22/Beijing_Babbler%2C_Zhengzhou%2C_Henan%2C_China_12.jpg',
+    updated_at = now()
+WHERE bird_id = 279 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/7123445f-8f06-480a-87ba-30a1a021f536.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/d/da/Asian_Stubtail_imported_from_iNaturalist_photo_296965690_on_20_June_2024.jpg',
+    updated_at = now()
+WHERE bird_id = 283 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/b8e8b163-2a69-430a-983b-6fdf821237e0.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Goldcrest_1.jpg',
+    updated_at = now()
+WHERE bird_id = 303 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/e96f6123-1449-468d-8949-3cc29b249278.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Agropsar_sturninus_-_Kent_Ridge_Park.jpg',
+    updated_at = now()
+WHERE bird_id = 308 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/de00c1c0-af44-4a96-a5ea-56b6a8cbf24a.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Coccothraustes_coccothraustes.jpg',
+    updated_at = now()
+WHERE bird_id = 381 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/d8791882-27fd-4477-bb1d-ffa8cb1ebc5e.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/6/64/The_brown-cheeked_rail_or_eastern_water_rail.jpg',
+    updated_at = now()
+WHERE bird_id = 449 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/0dc60dc7-5aad-4934-8a73-398d2c567e19.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/9/92/Coal_tit_UK09.JPG',
+    updated_at = now()
+WHERE bird_id = 464 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/db76de2e-faca-4c91-a1ee-8358f3c4fa5c.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/7/72/Luscinia_sibilans_-_Khao_Yai.jpg',
+    updated_at = now()
+WHERE bird_id = 472 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/1c169351-14b6-478a-a531-db789486e250.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/f/f1/GRAY-CROWNED-WARBLER-KHONOMA.jpg',
+    updated_at = now()
+WHERE bird_id = 499 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/ccae417c-caea-46f8-b7ac-0013a80dbd0b.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Bubo_scandiacus_%28Linnaeus%2C_1758%29_Male.jpg',
+    updated_at = now()
+WHERE bird_id = 513 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/b96dba87-48c5-4d09-a4fc-c6b93194a419.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Hierococcyx_hyperythrus.jpg',
+    updated_at = now()
+WHERE bird_id = 542 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/41eafc4b-c27b-48e9-83c9-71dd43f2ebce.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/3/36/Luscinia_calliope_-_Pak_Chong_2.jpg',
+    updated_at = now()
+WHERE bird_id = 544 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/9cf26537-9170-476f-8d33-f1004969a31d.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/6/61/Oceanites_oceanicusPCCA20070623-3634B.jpg',
+    updated_at = now()
+WHERE bird_id = 568 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/9f3c1394-687c-4abc-a0ef-870b69ae6a7f.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/7/78/Japanese_Robin_9625.jpg',
+    updated_at = now()
+WHERE bird_id = 585 AND is_thumb = TRUE;
+
+UPDATE public.bird_image
+SET object_key = 'raw/71c3aba8-c52b-4655-bdb0-8a12b225bc61.jpg',
+    original_url = 'https://upload.wikimedia.org/wikipedia/commons/2/2e/Mehlschwalbe_Delichon_urbicum.jpg',
+    updated_at = now()
+WHERE bird_id = 591 AND is_thumb = TRUE;
---
+++ src/main/resources/db/migration/V55__update_scientific_name.sql
@@ -0,0 +1,3 @@
+UPDATE bird
+SET genus_eng = 'Agropsar', genus_kor = '북방쇠찌르레기속', scientific_name = 'Agropsar sturninus'
+WHERE id = 308;
