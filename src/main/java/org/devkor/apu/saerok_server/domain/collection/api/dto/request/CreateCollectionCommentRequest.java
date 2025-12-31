50d6b81781049f664c40f7821e9550495249fc0d
pizzazoa <gkswns0716@naver.com>
2026-01-01 01:52:32 +0900

    feat(coll): 대댓글 기능을 위해 댓글 작성 요청 body에 comment_id 필드 추가

--- src/main/java/org/devkor/apu/saerok_server/domain/collection/api/dto/request/CreateCollectionCommentRequest.java
+++ src/main/java/org/devkor/apu/saerok_server/domain/collection/api/dto/request/CreateCollectionCommentRequest.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CreateCollectionCommentRequest(
         @Schema(description = "댓글 내용", example = "멋진 사진이네요!", requiredMode = Schema.RequiredMode.REQUIRED)
-        String content
+        String content,
+
+        @Schema(description = "부모 댓글 ID (대댓글 작성 시)", example = "123", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+        Long commentId
 ) {
 }
