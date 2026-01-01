1ebccb8780c120bcffe48694d15b1a8744f8eea0
pizzazoa <gkswns0716@naver.com>
2026-01-01 17:42:10 +0900

    feat: admin에서 댓글 삭제에도 soft delete 적용
    추후 삭제한 것과 신고로 밴된 것의 차별을 두기 위해 status = BANNED 적용

--- src/main/java/org/devkor/apu/saerok_server/domain/admin/report/application/AdminReportCommandService.java
+++ src/main/java/org/devkor/apu/saerok_server/domain/admin/report/application/AdminReportCommandService.java
@@ -153,10 +153,15 @@ public class AdminReportCommandService {
         // 1) 관련 신고 정리
         commentReportRepository.deleteByCommentId(commentId);
 
-        // 2) 댓글 삭제
+        // 2) 댓글 삭제 (대댓글이 있으면 soft delete, 없으면 hard delete)
         UserBirdCollectionComment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new NotFoundException("해당 댓글이 존재하지 않아요"));
-        commentRepository.remove(comment);
+
+        if (commentRepository.hasReplies(commentId)) {
+            comment.ban();
+        } else {
+            commentRepository.remove(comment);
+        }
 
         // 3) 감사 기록
         User admin = userRepository.findById(adminUserId)
