48be0d67ca390ea5cd2ae04731b6b5fd3cb3a2a3
pizzazoa <gkswns0716@naver.com>
2026-01-14 18:44:16 +0900

    refactor: 페이징 커맨드 dto를 공통 유틸로 묶어서 추상화

--- src/main/java/org/devkor/apu/saerok_server/domain/collection/application/dto/CommentQueryCommand.java
+++ src/main/java/org/devkor/apu/saerok_server/domain/collection/application/dto/CommentQueryCommand.java
@@ -1,22 +1,8 @@
 package org.devkor.apu.saerok_server.domain.collection.application.dto;
 
+import org.devkor.apu.saerok_server.global.shared.util.Pageable;
+
 public record CommentQueryCommand(
         Integer page,
         Integer size
-) {
-    public boolean hasValidPagination() {
-        if ((page != null && size == null) || (page == null && size != null)) {
-            return false;
-        }
-
-        if (page == null) { // page == null && size == null
-            return true;
-        }
-
-        return page >= 1 && size >= 1;
-    }
-
-    public boolean hasPagination() {
-        return page != null && size != null;
-    }
-}
+) implements Pageable {}
