a81b15bf5fab0d18778cdb6c8bbe833ea39a9a6d
pizzazoa <gkswns0716@naver.com>
2026-02-06 17:57:19 +0900

    feat: 알림 설정 도메인의 api에도 플랫폼 필드 추가

--- src/main/java/org/devkor/apu/saerok_server/domain/notification/application/dto/ToggleNotificationSettingCommand.java
+++ src/main/java/org/devkor/apu/saerok_server/domain/notification/application/dto/ToggleNotificationSettingCommand.java
@@ -1,9 +1,11 @@
 package org.devkor.apu.saerok_server.domain.notification.application.dto;
 
+import org.devkor.apu.saerok_server.domain.notification.core.entity.DevicePlatform;
 import org.devkor.apu.saerok_server.domain.notification.core.entity.NotificationType;
 
 public record ToggleNotificationSettingCommand(
         Long userId,
         String deviceId,
+        DevicePlatform platform,
         NotificationType type
 ) {}
